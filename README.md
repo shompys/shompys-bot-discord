9507ed1184fb17c4c4f894f0d053885497184271
jonathan <jonathangomezit@gmail.com>
2021-06-14 22:11:54 -0300

    cleaning

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 _Aca encontraremos la documentación y los comandos que se iran creando en algún momento. Claro que sí!!_
 
+
 ![image](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqi2QexGWw3s7hZLfLQnZwxVO6CB56OXIvQxC3SnwPRo3cU_7nsMOcY7xljvR5u8E7v2c&usqp=CAU)
+
+##  Comandos basicos: 
+```
+!hola       - saludo al bot
+!doc        - documentación del bot 
+!avatar     - tu imagen de perfil
+!ping       - ping
+```
--- src/app.js
+++ src/app.js
@@ -5,16 +5,12 @@ import { commandsGeneric } from './controllers/commandsGeneric';
 import { commandsBotInit } from './controllers/commandsBotInit';
 
 const client = new Client();
-// {
-//     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE]
-// }
 
 //client bot init
 client.on('ready', commandsBotInit(client));
 
 // client discord commands
 client.on('message', commandsGeneric);
-
 
 client.login(process.env.DISCORD_ACCESS_TOKEN);
 
